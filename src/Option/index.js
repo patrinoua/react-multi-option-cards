1d6ec40a4ad4593edd267aa5ba8e9ef63168e5a5
Mona Lisa <patrinoua@gmail.com>
2019-05-24 03:07:00 +0200

    add blue icons

--- src/Option/index.js
+++ src/Option/index.js
@@ -2,13 +2,33 @@ import React from 'react'
 import { Container, Description, Icon, Input } from './elements.js'
 
 export default class Option extends React.Component {
+  state = {
+    isActive: false
+  }
+  toggleActive = e => {
+    this.setState({
+      isActive: !this.state.isActive
+    })
+  }
   render() {
     const { icon, children, checkedOptions } = this.props
+    const { isActive } = this.state
     return (
       <React.Fragment>
-        <Input type="checkbox" id={icon} onClick={e => checkedOptions(e)} />
+        <Input
+          type="checkbox"
+          id={icon}
+          onClick={e => {
+            checkedOptions(e)
+            this.toggleActive(e)
+          }}
+        />
         <Container htmlFor={icon} name={icon} id={icon}>
-          <Icon src={`/assets/${icon}.png`} alt="musicIcon" />
+          {isActive ? (
+            <Icon src={`/assets/blueIcons/${icon}.png`} alt="musicIcon" />
+          ) : (
+            <Icon src={`/assets/greyIcons/${icon}.png`} alt="musicIcon" />
+          )}
           <Description>{children}</Description>
         </Container>
       </React.Fragment>
